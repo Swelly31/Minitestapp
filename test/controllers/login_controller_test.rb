c688917471c3854c7fb798081fbba6da138e61a6
THIERRY OSPITAL <40397238+Swelly31@users.noreply.github.com>
2018-11-05 23:44:07 +0100

    dernier push

--- test/controllers/login_controller_test.rb
+++ test/controllers/login_controller_test.rb
@@ -1,7 +1,53 @@
 require 'test_helper'
 
 class LoginControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test 'Homepage links' do
+    get '/'
+    assert_response :success
+    assert_select "a[href=?]", '/login/new'
+    assert_select "a[href=?]", '/login/check'
+  end
+
+
+  test 'Can create new user' do
+    get '/login/new'
+    assert_response :success
+
+    post '/login/new',
+      params: {user:{first_name: "Thierry", last_name: "Ospi", email:"ospi@td.fr", password: "test"}}
+      assert_response :success      
+  end
+
+  test 'Cannot create empty user' do
+    post '/login/new',
+    params: {user:{first_name: "", last_name: "", email:"", password: ""}}
+    assert_redirected_to '/login/blank'
+  end     
+
+  test 'Login success' do
+    post '/login/check',
+    params: {'/login/login':{email: "aaa", password: "aaa"}}
+    assert_redirected_to '/login/success'
+  end
+
+  test 'Login failed' do
+    post '/login/check',
+    params: {'/login/login':{email: "", password: ""}}
+    assert_redirected_to '/login/failed'
+  end
+
+  test 'Navbar elements' do
+    get '/'
+    assert_select 'nav/a', :href => '/'
+    assert_select 'nav/a', :href => '/login/new'
+    assert_select 'nav/a', :href => '/login/check'
+  end
+
+  test 'Page club accessible seulement aux utilisateurs connectés' do
+  get '/'
+  #assert_select 'h3', "Your account" Won't work, @current_user not implemented?
+  assert_response :success
+  end
+
 end
